79391eab84768e373e631511b3514b3f778578de
Balamithran228 <73731940+Balamithran228@users.noreply.github.com>
2023-10-24 18:34:39 +0530

    Update settings.py

--- text_to_html/settings.py
+++ text_to_html/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^*^cgkpp-ic^das7t7_=^ihg8vh635*8o&20(sfcnpcqz(8m4f'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
